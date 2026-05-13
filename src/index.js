22e5dd24b65670895efe615213c9c91582c6505f
syndravin-jpg <syndravin@gmail.com>
2026-05-13 20:33:06 +0300

    index await kaldirildi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import 'dotenv/config';
-
-async function main() {
-  await import('./server.js');
-  await import('./workers/playerTracker.js');
-  console.log('🚀 Server + Worker birlikte başlatıldı');
-}
-
-main().catch(console.error);
+import './server.js';
+import './workers/playerTracker.js';
+console.log('🚀 Server + Worker birlikte başlatıldı');
